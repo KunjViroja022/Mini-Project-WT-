951e2b4ad5a5418e2527640d828a84eacbd1a8f2
KunjViroja022 <23it138@charusat.edu.in>
2024-09-01 14:33:07 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript for handling "Add to Cart" button clicks
+document.querySelectorAll('.add-to-cart').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});
